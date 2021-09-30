51604c8f86f055e555a4fefb3716d7f00acaa0e2
unknown <chintanavyasree@gmail.com>
2021-09-30 10:17:42 +0530

    uc1 problem

--- Employee.java
+++ Employee.java
@@ -1,6 +1,12 @@
  public class Employee{
     public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage Computation");
+      System.out.println("Welcome to Employee Wage Computation");
+     int isfulltime=1;
+          double empcheck=Math.floor(Math.random() *10) % 2;
+        if (empcheck == isfulltime) 
+	System.out.println(" Employee is present ");
+       else 
+       System.out.println(" Employee is absent ");
 	
 	}
  }
